bd1df31041d7d4764081f6290e51dcc315b93072
Trust Okerezi <trustokerezi42@gmail.com>
2021-07-20 13:53:54 +0100

    first commit

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/interior-1.jpg';
+import ImageTwo from '../images/interior-2.jpg';
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraghOne: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt vitae accusamus maiores iste iure ratione dolorum, porro aspernatur quisquam sapiente eum minus cumque ut dignissimos ipsam magni qui autem ipsa?',
+    paragraphTwo: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis natus at, explicabo iusto sint cumque totam laboriosam obcaecati officia fugiat?',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+};
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraghOne: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt vitae accusamus maiores iste iure ratione dolorum, porro aspernatur quisquam sapiente eum minus cumque ut dignissimos ipsam magni qui autem ipsa?',
+    paragraphTwo: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis natus at, explicabo iusto sint cumque totam laboriosam obcaecati officia fugiat?',
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300
+};
